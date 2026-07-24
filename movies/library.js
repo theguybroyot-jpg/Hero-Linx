f0bf54d4d4121aacbbcaef985b33f4a5919ddbe0
theguybroyot-jpg <theguybroyot@gmail.com>
2026-07-23 18:16:33 -0600

    Create library.js

---
+++ movies/library.js
@@ -0,0 +1,54 @@
+const TMDB_API_KEY = "YOUR_NEW_TMDB_API_KEY";
+
+const library = [
+
+    // TV Shows
+
+    {
+        imdb: "tt0934814",
+        type: "tv"
+    },
+
+    {
+        imdb: "tt0386676",
+        type: "tv"
+    },
+
+    {
+        imdb: "tt0491738",
+        type: "tv"
+    },
+
+    {
+        imdb: "tt4574334",
+        type: "tv"
+    },
+
+    {
+        imdb: "tt9335498",
+        type: "tv"
+    },
+
+    // Movies
+
+    {
+        imdb: "tt2250912",
+        type: "movie"
+    },
+
+    {
+        imdb: "tt6320628",
+        type: "movie"
+    },
+
+    {
+        imdb: "tt10872600",
+        type: "movie"
+    },
+
+    {
+        imdb: "tt22084616",
+        type: "movie"
+    }
+
+];
